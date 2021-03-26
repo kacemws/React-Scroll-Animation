a031a7eb07a934e098a7f437ebea11b0653bdaee
kacemws <berras.belkacem@gmail.com>
2021-03-26 19:56:12 +0100

    adding a nav menu and an auth section in the header

--- src/Components/Header/Header.tsx
+++ src/Components/Header/Header.tsx
@@ -1,3 +1,4 @@
+import { Title } from "../Typographie";
 import "./Header.scss";
 
 export const Header: React.FC<{}> = ({ children, ...rest }) => {
@@ -6,8 +7,30 @@ export const Header: React.FC<{}> = ({ children, ...rest }) => {
       <div className="header-logo">
         <img src="https://cdn.kiostore.net/file/kiostore/KIO+.svg" alt="logo" />
       </div>
-      <div className="header-nav"></div>
-      <div className="header-auth"></div>
+      <div className="header-nav">
+        <nav>
+          <ul>
+            <li>
+              <Title>Home</Title>
+            </li>
+            <li>
+              <Title>Kiosk</Title>
+            </li>
+            <li>
+              <Title>Pricing</Title>
+            </li>
+            <li>
+              <Title>About</Title>
+            </li>
+          </ul>
+        </nav>
+      </div>
+      <div className="header-auth">
+        <Title>Sign in</Title>
+        <div className="button">
+          <Title>Sign up</Title>
+        </div>
+      </div>
     </div>
   );
 };
